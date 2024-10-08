cb720d35d4f9cb6a3ed5d160137fd920ff6844cc
Adnane Kharkhach <Archertectures@hotmail.com>
2024-10-08 13:20:38 +0200

    Renamed directories

---
+++ rename_directories.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+# Rename directories
+mv 'New Project Name 1' 'Project 2'
+mv 'New Project Name 2' 'Project 3'
+mv 'New Project Name 3' 'Project 4'
+mv 'New Project Name 4' 'Project 5'
+mv 'New Project Name 5' 'Project 6'
+mv 'New Project Name 6' 'Project 7'
+
+echo "Directories renamed successfully!"
